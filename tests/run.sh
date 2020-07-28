6f417c4bf1502a8331a3fc487d23ace4f1f8a875
ssi91 <sergeev.s.i1991@gmail.com>
2020-07-28 21:31:16 +0700

    remove port-forvarding

--- tests/run.sh
+++ tests/run.sh
@@ -6,7 +6,7 @@ echo "Run tests custom"
 # Prepare network & run container
 docker network create hw-test-net-lint
 docker run -d -v $(pwd):/srv -v /var/run/docker.sock:/tmp/docker.sock \
-    -e DOCKER_HOST=unix:///tmp/docker.sock --cap-add=NET_ADMIN -p 33433:22 --privileged \
+    -e DOCKER_HOST=unix:///tmp/docker.sock --cap-add=NET_ADMIN --privileged \
     --device /dev/net/tun --name hw-test-lint --network hw-test-net-lint $DOCKER_IMAGE
 # Show versions & run tests
 docker exec hw-test-lint bash ./tests/packer_validation.sh
